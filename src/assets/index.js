a30ab84c2c1a6c5366bdf5394a1bc2059ceaae1c
oussamaSeddini <oussamasdn1127@gmail.com>
2024-04-02 07:47:32 +0100

    Creat and Update the base of the project

---
+++ src/assets/index.js
@@ -0,0 +1,64 @@
+import logo from "./logo.svg";
+import VRandAR from "./VRandAR.png";
+import Industry from "./Industry.png";
+import ML from "./ML.png";
+import web from "./web.png";
+import github from "./github.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+
+import css from "./tech/css.png";
+import ue from "./tech/ue.png";
+import figma from "./tech/figma.png";
+import gcp from "./tech/gcp.png";
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import mongodb from "./tech/mongodb.png";
+import mariadb from "./tech/mariadb.png";
+import reactjs from "./tech/reactjs.png";
+import mssql from "./tech/mssql.png";
+import aws from "./tech/aws.svg";
+import python from "./tech/python.png";
+import tf from "./tech/tensorflow.png";
+import threejs from "./tech/threejs.svg";
+
+import meta from "./company/meta.png";
+import shopify from "./company/shopify.png";
+import starbucks from "./company/starbucks.png";
+import rmb from "./company/rmb.png";
+
+import carrent from "./carrent.png";
+import jobit from "./jobit.png";
+import tripguide from "./tripguide.png";
+
+export {
+  logo,
+  ML,
+  Industry,
+  VRandAR,
+  web,
+  github,
+  menu,
+  close,
+  css,
+  ue,
+  figma,
+  gcp,
+  html,
+  javascript,
+  mongodb,
+  mariadb,
+  reactjs,
+  mssql,
+  aws,
+  python,
+  tf,
+  threejs,
+  meta,
+  shopify,
+  starbucks,
+  rmb,
+  carrent,
+  jobit,
+  tripguide,
+};
